218d9a03da2bb8a5e63e56340fc8316c5087481f
michael <michael@12southmusic.com>
2014-02-13 14:24:02 -0600

    More vid thumb function changes

--- lib/custom.php
+++ lib/custom.php
@@ -4,9 +4,11 @@
  */
 
 
-function vidthumb_func( $post_id ) {
-     $thevidthumb = get_video_thumbnail( $post_id );
-     return $thevidthumb;
+function vidthumb_funct($atts) {
+   extract(shortcode_atts(array(
+      'id' => 200,
+   ), $atts));
+   $thevidthumb = get_video_thumbnail( $id );
+   return $thevidthumb;
 }
 add_shortcode('vidthumb', 'vidthumb_func');
-
