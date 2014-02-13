1ce75bb117b60d0bafed699595db7cd4f958456e
michael <michael@12southmusic.com>
2014-02-13 14:39:26 -0600

    Took short code out of functions so I can screw up a plugin instead

--- lib/custom.php
+++ lib/custom.php
@@ -4,11 +4,3 @@
  */
 
 
-function vidthumb_func($atts) {
-   extract(shortcode_atts(array(
-      'id' => 200,
-   ), $atts));
-   $thevidthumb = get_video_thumbnail( $id );
-   return $thevidthumb;
-}
-add_shortcode('vidthumb', 'vidthumb_func');
