37351b9088e1435ec533ffabdac1d751050abefc
michael <michael@12southmusic.com>
2014-02-13 14:10:26 -0600

    Adding a vidthumb function?

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,11 @@
 /**
  * Custom functions
  */
+
+
+function vidthumb_func( $post_id ) {
+     $thevidthumb = get_video_thumbnail( $post_id );
+     return $thevidthumb;
+}
+add_shortcode('vidthumb', 'vidthumb_func');
+
